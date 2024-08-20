4c1733a7d2ea0e86df42d77c9b04efe04ca85d69
max6971 <cmytt@mail.ru>
2024-08-20 13:24:57 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from app import routes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
@@ -13,4 +13,3 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from app import routes
