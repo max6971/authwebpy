3de753abbfa900fc1f2ae6175b490e3b3729e3be
max6971 <cmytt@mail.ru>
2024-08-20 13:14:06 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get((user_id))
+    return User.query.get(int(user_id))
 
-class User(db.model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), uniqui=True, nullable=False)
     email = db.Column(db.String(40), uniqui=True, nullable=False)
     password_hash = db.Column(db.String(60), nullable=False)
